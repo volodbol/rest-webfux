423d5a92f4336b599ba2bcc1dcc9874173ed7aaf
Volodymyr Bolshakov <volodbol@gmail.com>
2023-01-24 13:58:27 +0200

    feat: add models

---
+++ src/main/java/com/example/reactive/rest/model/Task.java
@@ -0,0 +1,32 @@
+package com.example.reactive.rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    private Long id;
+
+    private String description;
+
+    private Long projectId;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
